8461315bbab9af78c5896caeee5411fae80037a4
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-16 16:30:13 +0700

    App launching screen and icons

---
+++ targets/wallet-droid/SplashActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Content;
+
+namespace Wallet.Droid
+{
+    [Activity(NoHistory = true, Label = "@string/app_name", Icon = "@mipmap/ic_launcher", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Android.OS.Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(new Intent(this, typeof(MainActivity)));
+        }
+    }
+}
