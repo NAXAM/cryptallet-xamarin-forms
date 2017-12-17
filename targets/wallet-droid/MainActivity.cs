7a146dab299528060ae9a0e9d6e13688c763955e
Vu Duc Tuyen <tuyen@naxam.net>
2017-12-17 19:43:53 +0700

    Missing files

---
+++ targets/wallet-droid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Wallet.Forms.Bootstraps;
+using Plugin.SecureStorage;
+using Wallet.Controls.Droid.Renderers;
+using Acr.UserDialogs;
+
+namespace Wallet.Droid
+{
+    [Activity(Label = "@string/app_name", Icon = "@mipmap/ic_launcher", Theme = "@style/MyTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            PreserveCustomRenderers();
+
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            SecureStorageImplementation.StoragePassword = "$AES-128-CBC$FRz75wk/LvdAyOMyY9Wibw==$GNzheuuNsOkkBHG3hFSpA37UAQ1TDWQH0ncZcR2+7r4=";
+            LoadApplication(new WalletApplication());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        void PreserveCustomRenderers()
+        {
+            CustomEntryRenderer.Preserve();
+            CustomImageRenderer.Preserve();
+
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+        }
+    }
+}
---
+++ targets/wallet-ios/AppDelegate.cs
@@ -0,0 +1,53 @@
+﻿
+using Foundation;
+using UIKit;
+using Wallet.Forms.Bootstraps;
+using Wallet.Controls.iOS;
+using Wallet.Controls.iOS.Renderers;
+
+namespace Wallet.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            PreserveCustomRenderers();
+
+            SetupTheme();
+
+            global::Xamarin.Forms.Forms.Init();
+
+            // Code for starting up the Xamarin Test Cloud Agent
+#if DEBUG
+            Xamarin.Calabash.Start();
+#endif
+            LoadApplication(new WalletApplication());
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        void PreserveCustomRenderers()
+        {
+            CustomEntryRenderer.Preserve();
+            CustomImageRenderer.Preserve();
+
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+        }
+
+        void SetupTheme()
+        {
+            var colorPrimary = UIColor.FromRGB(0x35 / 255f, 0x74 / 255f, 0xFA / 255f);
+            var colorTextPrimary = UIColor.White;
+
+            UINavigationBar.Appearance.BackgroundColor = colorPrimary;
+            UINavigationBar.Appearance.BarTintColor = colorPrimary;
+            UINavigationBar.Appearance.TintColor = colorTextPrimary;
+
+            UINavigationBar.Appearance.TitleTextAttributes = new UIStringAttributes()
+            {
+                ForegroundColor = colorTextPrimary
+            };
+        }
+    }
+}
